b8079f01ade10a670a56c4a5b510128e69eef571
BT_4N2_23 <amejia9149@bths.edu>
2022-11-14 12:34:15 -0500

    added planner and random method

--- src/heistStory.java
+++ src/heistStory.java
@@ -10,21 +10,57 @@ public class heistStory {
     public String method(String hello){
         if ("random".equals(this.outcome)) {
             choice = 4;
-        } else if ("robber".equals(this.outcome)) {
+            random(choice);
+        }
+        if ("robber".equals(this.outcome)) {
             choice = 1;
-            return robber(outcome);
+            return robber();
         } else if ("planner".equals(outcome)) {
             choice = 2;
+            return planner();
         } else if ("muscle".equals(outcome)) {
             choice = 3;
         }
         return hello;
     }
-
-    private String robber(String first){
-        first="robber";
+    private void random(int random){
+        int four;
+        if(random==4){
+            four=(int)(Math.random()*3)+1;
+            if(four==1){
+                outcome="robber";
+            } else if (four==2) {
+                outcome="planner";
+            } else if (four==3) {
+                outcome="muscle";
+            }
+        }
+    }
+    public String planner(){
+        String second="planner";
+        String task="";
+        if(second.equals(outcome)){
+            task= "You picked the hardest task. You will plan the task. Where do you want to rob?\nbank, gas station, rich person";
+        }
+        return task;
+    }
+    public String planPlace(String which){
+        String decide="";
+        if(Objects.equals(which, "bank")){
+            decide="Bet. We will rob a bank. Now you have to decide how much money from the bank you want?";
+        }
+        if(Objects.equals(which,"gas station")){
+            decide="Bet. We will rob a gas station. Now you have to decide what you want from the gas station";
+        }
+        if(Objects.equals(which,"rich person")){
+            decide="Bet. We will rob a gas Station\nNow you must decide how to sneak into the place";
+        }
+        return decide;
+    }
+    private String robber(){
+        String first = "robber";
         if(Objects.equals(first, outcome)){
-            return "You and your friends are in the car. As you are ready to leave, you can between a machete or a gun. Which one do you pick?";
+            return "You and your friends are in the car. As you are ready to leave, you can choose between a machete or a gun. Which one do you pick?";
 
         }
         return "Hello";
@@ -32,10 +68,20 @@ public class heistStory {
     public String MoG(String which){
         String MoG="";
         if(which.equals("gun")){
-            MoG= "Gun it is! Now you enter the bank and yell \"Everybody Down!\"";
+            MoG= "Gun it is! Now you enter the bank and yell \"Everybody Down!\"\nYou can use the gun to hold a hostage. Do you want to hold a hostage";
+            return MoG+choiceGun(outcome);
         } else if(which.equals("machete")){
             MoG= "machete is it! Now you enter the bank and yell Freeze! Suddenly someone grabs the machete and you DIE! ";
         }
-        return MoG;
+        return choiceGun("gun")+MoG;
+    }
+    public String choiceGun(String choice){
+        String noOrYes="";
+            if(choice.equals("yes")){
+                noOrYes="Alright. You have now taken a hostage captive. The banker has negotiated with you get the key to the vault";
+            } else if(choice.equals("no")){
+                noOrYes="Alright. You will not take a hostage. The banker thinks you're not intimidating enough\nHe calls the police and you go to prison";
+            }
+        return noOrYes;
     }
 }
--- src/heistStoryRunner.java
+++ src/heistStoryRunner.java
@@ -6,14 +6,21 @@ public class heistStoryRunner {
 
     public static void main(String[] args) {
         Scanner s= new Scanner(System.in);
-        System.out.println("Option 1: robber- this person will be the one to enter a bank and steal the money\n "+"Option 2: planner- This person will plan the heist\n"+"Option 3: The muscle- This person will intimidate the guards \n"+"Option 4: random- If you are unsure of what to do enter shuffle and the program will randomly pick for you ");
+        System.out.println("Option 1: robber- this person will be the one to enter a bank and steal the money\n"+"Option 2: planner- This person will plan the heist\n"+"Option 3: the muscle- This person will intimidate the guards \n"+"Option 4: random- If you are unsure of what to do enter shuffle and the program will randomly pick for you ");
         System.out.println("Welcome to my heist story! Pick the role you want :). Have fun!");
         String outcomes= s.nextLine();
         heistStory user= new heistStory(outcomes);
                 System.out.println(user.method(outcomes));
                 String choice  = s.nextLine();
-                System.out.println(user.MoG(choice));
+                System.out.println(user.planPlace(choice));
+
+
     }
 }
 
+
+/* System.out.println(user.MoG(choice));
+                String hos= s.nextLine();
+                System.out.println(user.choiceGun(hos));
+ */
 
